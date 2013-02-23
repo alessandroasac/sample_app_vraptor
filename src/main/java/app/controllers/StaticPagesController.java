637d746d91254d04f1b3460ed2e0002cb44d481d
Alessandro Costa <alessandro.asac@gmail.com>
2013-02-23 01:34:43 -0300

    Add a StaticPages controller

---
+++ src/main/java/app/controllers/StaticPagesController.java
@@ -0,0 +1,17 @@
+package app.controllers;
+
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
+
+@Resource
+public class StaticPagesController {
+
+  public void home() {
+    
+  }
+
+  public void help() {
+
+  }
+}
---
+++ src/main/webapp/WEB-INF/jsp/staticPages/home.jsp
@@ -0,0 +1,2 @@
+<h1>StaticPages#home</h1>
+<p>Find me in webapp/WEB-INF/staticPages/home.jsp</p>
